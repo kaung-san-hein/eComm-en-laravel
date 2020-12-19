d4cd8ebd569645f16409da00bb21e35afbdad855
kaung-san-hein <kaungsanhein2019@gmail.com>
2020-12-19 15:41:38 +0630

    home page slider with products done

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -16,6 +16,18 @@
         height: 500px;
         padding-top: 100px;
     }
+
+    img.slider-img {
+        height: 400px !important;
+    }
+
+    .custom-product {
+        height: 600px;
+    }
+
+    .slider-text {
+        background-color: #35443585 !important;
+    }
 </style>
 
 <body>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,38 @@
+@extends('master')
+
+@section('content')
+
+<div class="custom-product">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            @foreach ($products as $item)
+            <div class="carousel-item {{$item['id'] == 1 ? 'active' : ''}}">
+                <img class="slider-img" src="{{$item['gallery']}}">
+                <div class="carousel-caption d-none d-md-block slider-text">
+                    <h3>{{$item['name']}}</h3>
+                    <p>{{$item['description']}}</p>
+                </div>
+            </div>
+            @endforeach
+        </div>
+
+        <!-- Left and right controls -->
+        <a style="background-color: #35443585 !important;" class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a style="background-color: #35443585 !important;" class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+@endsection
